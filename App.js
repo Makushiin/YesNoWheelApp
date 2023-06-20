ce718963b81295b3ea6e2e98d9dce13e8cdacd6e
Makushiin <maxinemayor@gmail.com>
2023-06-19 18:50:54 -0700

    Changed animation and result bug

--- App.js
+++ App.js
@@ -1,73 +1,65 @@
-import React, { useState } from 'react';
-import { View, Text, TouchableOpacity, Animated } from 'react-native';
-import { styles } from './styles';
-import CircleDivider from './CircleDivider';
+import React, { useState } from "react";
+import { View, Text, TouchableOpacity } from "react-native";
+import * as Animatable from "react-native-animatable";
+import { Easing } from "react-native";
+import { styles } from "./styles";
+import CircleDivider from "./CircleDivider";
 
 const App = () => {
-  const [rotateAnimation] = useState(new Animated.Value(0));
-  const [result, setResult] = useState('');
-  const [isSpinning, setIsSpinning] = useState(false); // Track the spinning status
+  const [result, setResult] = useState("");
+  const [isSpinning, setIsSpinning] = useState(false);
 
   const spinWheel = () => {
     if (isSpinning) {
       return; // Ignore if spinning is already in progress
     }
 
-    // Reset the result text and rotation animation
-    setResult('');
-    rotateAnimation.setValue(0);
-
-    // Update the spinning status to prevent multiple spins
+    setResult("");
     setIsSpinning(true);
 
-    // Perform the spinning animation
-    const minDegrees = 1800;
-    const maxDegrees = 3600;
-    const randomDegrees = Math.floor(Math.random() * (maxDegrees - minDegrees + 1)) + minDegrees;
+    const segmentCount = 8;
+    const minDegrees = 2080;
+    const maxDegrees = 2860;
+    const randomDegrees =
+      Math.floor(Math.random() * (maxDegrees - minDegrees + 1)) + minDegrees;
 
-    Animated.timing(rotateAnimation, {
-      toValue: randomDegrees / 360,
-      duration: 3000,
-      useNativeDriver: true, // Set useNativeDriver to true
-    }).start(() => {
-      // Calculate the selected option based on the final rotation value
-      const rotationValue = rotateAnimation._value % 1; // Get the decimal part of the rotation value
-      const segmentCount = 8; // Number of segments in the wheel
-      const topSegmentIndex = Math.floor(rotationValue * segmentCount);
+    const duration = 3000;
 
-      // Find the corresponding text for the top segment
-      const texts = ['YES', 'NO']; // Alternating text values
-      const topSegmentText = texts[topSegmentIndex % texts.length];
+    wheelRef
+      .animate({
+        from: { rotate: "0deg" },
+        to: { rotate: `${randomDegrees}deg` },
+        duration,
+        easing: Easing.inOut(Easing.quad), // Apply easing in and out effect
+      })
+      .then(() => {
+        const rotationValue = randomDegrees % 360;
+        const topSegmentIndex = Math.floor(
+          rotationValue / (360 / segmentCount)
+        );
 
-      // Update the result text with the top segment text
-      setResult(topSegmentText);
+        const texts = ["YES", "NO"];
+        const topSegmentText = texts[topSegmentIndex % texts.length];
 
-      // Update the spinning status to allow spinning again
-      setIsSpinning(false);
-    });
+        setResult(topSegmentText);
+        setIsSpinning(false);
+      });
   };
 
-  const interpolateRotating = rotateAnimation.interpolate({
-    inputRange: [0, 1],
-    outputRange: ['0deg', '360deg'],
-  });
-
-  const animatedStyle = {
-    transform: [
-      {
-        rotate: interpolateRotating,
-      },
-    ],
-  };
+  let wheelRef;
 
   return (
     <View style={styles.container}>
-      <Animated.View style={[styles.wheel, animatedStyle]}>
+      <Animatable.View ref={(ref) => (wheelRef = ref)} style={styles.wheel}>
         <View style={styles.circle}>
           <CircleDivider />
         </View>
-      </Animated.View>
-      <TouchableOpacity style={styles.button} onPress={spinWheel} disabled={isSpinning}>
+      </Animatable.View>
+      <TouchableOpacity
+        style={styles.button}
+        onPress={spinWheel}
+        disabled={isSpinning}
+      >
         <Text style={styles.spinText}>SPIN</Text>
       </TouchableOpacity>
       <Text style={styles.resultText}>{result}</Text>
